1b6d565cd9ad229848b1268524cec895cbbcde89
Jonathan Morgenstern <jonathan.morgenstern@webit.de>
2026-08-03 13:10:10 +0200

    [FEATURE] fill db with dummy data; implement routes for api access

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,96 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_03_104558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "agencies", force: :cascade do |t|
+    t.string "agency_id"
+    t.string "agency_lang"
+    t.string "agency_name"
+    t.string "agency_phone"
+    t.string "agency_timezone"
+    t.string "agency_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["agency_id"], name: "index_agencies_on_agency_id", unique: true
+  end
+
+  create_table "calendars", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "end_date"
+    t.boolean "friday"
+    t.boolean "monday"
+    t.boolean "saturday"
+    t.string "service_id"
+    t.string "start_date"
+    t.boolean "sunday"
+    t.boolean "thursday"
+    t.boolean "tuesday"
+    t.datetime "updated_at", null: false
+    t.boolean "wednesday"
+    t.index ["service_id"], name: "index_calendars_on_service_id", unique: true
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.bigint "agency_id", null: false
+    t.datetime "created_at", null: false
+    t.string "route_color"
+    t.string "route_id"
+    t.string "route_long_name"
+    t.string "route_short_name"
+    t.string "route_text_color"
+    t.integer "route_type"
+    t.datetime "updated_at", null: false
+    t.index ["agency_id"], name: "index_routes_on_agency_id"
+    t.index ["route_id"], name: "index_routes_on_route_id", unique: true
+  end
+
+  create_table "stop_times", force: :cascade do |t|
+    t.string "arrival_time"
+    t.datetime "created_at", null: false
+    t.string "departure_time"
+    t.integer "drop_off_type"
+    t.integer "pickup_type"
+    t.bigint "stop_id", null: false
+    t.integer "stop_sequence"
+    t.bigint "trip_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stop_id"], name: "index_stop_times_on_stop_id"
+    t.index ["trip_id"], name: "index_stop_times_on_trip_id"
+  end
+
+  create_table "stops", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "location_type"
+    t.string "stop_code"
+    t.string "stop_desc"
+    t.string "stop_id"
+    t.decimal "stop_lat"
+    t.decimal "stop_lon"
+    t.string "stop_name"
+    t.datetime "updated_at", null: false
+    t.integer "wheelchair_boarding"
+    t.string "zone_id"
+    t.index ["stop_id"], name: "index_stops_on_stop_id", unique: true
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "direction_id"
+    t.bigint "route_id", null: false
+    t.string "service_id", null: false
+    t.string "trip_headsign"
+    t.string "trip_id"
+    t.datetime "updated_at", null: false
+    t.integer "wheelchair_accessible"
+    t.index ["route_id"], name: "index_trips_on_route_id"
+    t.index ["trip_id"], name: "index_trips_on_trip_id", unique: true
+  end
+
+  add_foreign_key "routes", "agencies"
+  add_foreign_key "stop_times", "stops"
+  add_foreign_key "stop_times", "trips"
+  add_foreign_key "trips", "routes"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,40 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+agency = Agency.create!(
+  agency_id: "AG1",
+  agency_name: "Musterverkehr GmbH",
+  agency_url: "https://musterverkehr.example",
+  agency_timezone: "Europe/Berlin",
+  agency_lang: "de",
+  agency_phone: "+49 351 1234567"
+)
+
+stops = {
+  "S1" => Stop.create!(stop_id: "S1", stop_name: "Hauptbahnhof",  stop_lat: 51.0405, stop_lon: 13.7325, location_type: 0),
+  "S2" => Stop.create!(stop_id: "S2", stop_name: "Rathausplatz",   stop_lat: 51.0493, stop_lon: 13.7381, location_type: 0),
+  "S3" => Stop.create!(stop_id: "S3", stop_name: "Stadtpark",      stop_lat: 51.0521, stop_lon: 13.7502, location_type: 0),
+  "S4" => Stop.create!(stop_id: "S4", stop_name: "Universität",    stop_lat: 51.0290, stop_lon: 13.7222, location_type: 0)
+}
+
+route1 = Route.create!(route_id: "R1", agency: agency, route_short_name: "1", route_long_name: "Hauptbahnhof - Stadtpark", route_type: 3, route_color: "FF0000", route_text_color: "FFFFFF")
+route2 = Route.create!(route_id: "R2", agency: agency, route_short_name: "2", route_long_name: "Hauptbahnhof - Universität", route_type: 3, route_color: "0000FF", route_text_color: "FFFFFF")
+
+cal_weekday = Calendar.create!(service_id: "WD", monday: true, tuesday: true, wednesday: true, thursday: true, friday: true, saturday: false, sunday: false, start_date: "20260101", end_date: "20261231")
+cal_weekend = Calendar.create!(service_id: "WE", monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: true, sunday: true, start_date: "20260101", end_date: "20261231")
+
+trip1 = Trip.create!(trip_id: "T1", route: route1, service_id: "WD", trip_headsign: "Stadtpark", direction_id: 0)
+trip2 = Trip.create!(trip_id: "T2", route: route1, service_id: "WE", trip_headsign: "Stadtpark", direction_id: 0)
+trip3 = Trip.create!(trip_id: "T3", route: route2, service_id: "WD", trip_headsign: "Universität", direction_id: 0)
+
+[
+  [ trip1, "S1", "08:00:00", "08:00:00", 1 ],
+  [ trip1, "S2", "08:05:00", "08:06:00", 2 ],
+  [ trip1, "S3", "08:12:00", "08:12:00", 3 ],
+
+  [ trip2, "S1", "10:00:00", "10:00:00", 1 ],
+  [ trip2, "S2", "10:05:00", "10:06:00", 2 ],
+  [ trip2, "S3", "10:12:00", "10:12:00", 3 ],
+
+  [ trip3, "S1", "09:00:00", "09:00:00", 1 ],
+  [ trip3, "S4", "09:10:00", "09:10:00", 2 ]
+].each do |trip, stop_id, arr, dep, seq|
+  StopTime.create!(trip: trip, stop: stops[stop_id], arrival_time: arr, departure_time: dep, stop_sequence: seq)
+end
