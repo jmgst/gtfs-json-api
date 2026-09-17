adac3455dd58edd0615c4eedb79a35abe257e54c
Jonathan Morgenstern <jonathan.mgst@gmail.com>
2026-09-17 07:48:51 +0200

    [FEATURE] update routes_controller_test.rb

--- test/controllers/routes_controller_test.rb
+++ test/controllers/routes_controller_test.rb
@@ -1,19 +1,14 @@
 require "test_helper"
 
 class Api::V1::RoutesControllerTest < ActionDispatch::IntegrationTest
-  test "index gibt alle Routen als JSON zurück" do
+  test "index liefert alle Linien als JSON" do
     get api_v1_routes_url
     assert_response :success
     json = JSON.parse(response.body)
     assert_equal Route.count, json.length
   end
 
-  test "show liefert 404 für unbekannte route_id" do
-    get api_v1_route_url(id: "nicht_existent")
-    assert_response :not_found
-  end
-
-  test "show liefert genau die angefragte Route zurück" do
+  test "show liefert genau die angefragte Linie" do
     route = routes(:one)
 
     get api_v1_route_url(id: route.route_id)
@@ -24,5 +19,10 @@ class Api::V1::RoutesControllerTest < ActionDispatch::IntegrationTest
     assert_equal route.route_short_name, json["route_short_name"]
     assert_equal route.route_long_name, json["route_long_name"]
     assert_equal route.agency.agency_id, json["agency"]["agency_id"]
+  end
+
+  test "show liefert 404 bei unbekannter Linie" do
+    get api_v1_route_url(id: "nicht_existent")
+    assert_response :not_found
   end
 end
